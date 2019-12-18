77f700914868b0424cad1d7f2c125f9954a30d2f
allenwest24 <58156836+allenwest24@users.noreply.github.com>
2019-12-17 23:28:16 -0500

    Update test.pl

--- test.pl
+++ test.pl
@@ -18,36 +18,6 @@ sub run_bad {
     return $yy;
 }
 
-sub run_fib {
-    my ($bin, $xx) = @_;
-    my $yy = `timeout -s 9 5 ./$bin "$xx"`;
-    chomp $yy;
-    $yy =~ /^\s*fib\((\d+)\)\s*=\s*(\d+)\s*$/ or return -99;
-    $1 == $xx or return -2;
-    $yy = $2;
-    return int($yy);
-}
-
-ok(run_fib("cfib", 0) == 0, "cfib(0)");
-ok(run_fib("cfib", 1) == 1, "cfib(1)");
-ok(run_fib("cfib", 5) == 5, "cfib(5)");
-ok(run_fib("cfib", 10) == 55, "cfib(10)");
-ok(run_fib("cfib", 25) == 75025, "cfib(25)");
-
-# For bad input, print a message starting "Usage:"
-ok(run_bad("./cfib") =~ /Usage/i, "cfib()");
-ok(run_bad("./cfib -7") =~ /Usage/i, "cfib(-7)");
-
-ok(run_fib("afib", 0) == 0, "afib(0)");
-ok(run_fib("afib", 1) == 1, "afib(1)");
-ok(run_fib("afib", 5) == 5, "afib(5)");
-ok(run_fib("afib", 10) == 55, "afib(10)");
-ok(run_fib("afib", 25) == 75025, "afib(25)");
-
-# For bad input, print a message starting "Usage:"
-ok(run_bad("./afib") =~ /Usage/i, "afib()");
-ok(run_bad("./afib -7") =~ /Usage/i, "afib(-7)");
-
 sub run_calc {
     my ($bin, $op, $xx, $yy) = @_;
     my $zz = -99;
